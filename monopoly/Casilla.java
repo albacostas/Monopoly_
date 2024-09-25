dcf2987790d9537899ca6ab60b53db85fd9c4acb
Atenea <AteneaG-github@users.noreply.github.com>
2024-09-25 14:02:01 +0200

    fin prueba

--- monopoly/Casilla.java
+++ monopoly/Casilla.java
@@ -151,5 +151,4 @@ public class Casilla {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setPropiedades'");
     }
-    //HOLAAAAA
 }
