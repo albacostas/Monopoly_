3739b888678145060b65d1e2439dc0bd1f17a942
Alba Costas <157661250+albacostas@users.noreply.github.com>
2024-10-02 13:05:06 +0200

    Update MonopolyETSE.java

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -8,11 +8,8 @@ import java.util.Scanner;
 
 public class MonopolyETSE {
 
-    private static ArrayList<Jugador> jugadores = new ArrayList<>();
-    private static ArrayList<Avatar> avatares = new ArrayList<>();
-    private static ArrayList<ArrayList<Casilla>> tablero = new ArrayList<>();
     public static void main(String[] args) {
-        new Menu();
+      System.out.println(tablero);
     }
     
 }
