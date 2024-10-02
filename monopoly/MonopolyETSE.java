7e3f6217ba6ffc041e249422774a8f2c8374cfd0
Alba Costas <albacostasfernandez@gmail.com>
2024-10-02 13:08:19 +0200

    tablero

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -8,7 +8,13 @@ import java.util.Scanner;
 
 public class MonopolyETSE {
 
+    private static ArrayList<Jugador> jugadores = new ArrayList<>();
+    private static ArrayList<Avatar> avatares = new ArrayList<>();
     public static void main(String[] args) {
+    
+        Jugador banca = new Jugador();
+        Tablero tablero = new Tablero(banca);
+
       System.out.println(tablero);
     }
     
