4932c005ded1cc444ccde64a0e477d803cf929f7
Alba Costas <albacostasfernandez@gmail.com>
2024-10-08 00:49:11 +0200

    tabulacion de codigo

--- partida/Avatar.java
+++ partida/Avatar.java
@@ -5,7 +5,7 @@ import monopoly.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.management.AttributeChangeNotificationFilter;
+//import javax.management.AttributeChangeNotificationFilter;
 
 
 public class Avatar {
@@ -100,26 +100,26 @@ public class Avatar {
     }
 
 
-    private Casilla ObtenerSiguienteCasilla(ArrayList<ArrayList<Casilla>> casillas, Casilla casillaActual){
-        int posActual=casillaActual.getPosicion();
-        //Incrementamos la posción una unidad.
-        int nuevaPosicion=posActual+1;
-        //si la posición excede el número 40 (vuelta completada), regresamos a la casilla 1.
-        if(nuevaPosicion>40){
-            nuevaPosicion=1;
-        } 
-        //buscamos la nueva casilla actual basada en la posición
-        for(ArrayList<Casilla> lado : casillas){
-            for (Casilla casilla: lado){
-                if (casilla.getPosicion()==nuevaPosicion){
-                    return casilla;
-                }
-            }
-        }
+    // private Casilla ObtenerSiguienteCasilla(ArrayList<ArrayList<Casilla>> casillas, Casilla casillaActual){
+    //     int posActual=casillaActual.getPosicion();
+    //     //Incrementamos la posción una unidad.
+    //     int nuevaPosicion=posActual+1;
+    //     //si la posición excede el número 40 (vuelta completada), regresamos a la casilla 1.
+    //     if(nuevaPosicion>40){
+    //         nuevaPosicion=1;
+    //     } 
+    //     //buscamos la nueva casilla actual basada en la posición
+    //     for(ArrayList<Casilla> lado : casillas){
+    //         for (Casilla casilla: lado){
+    //             if (casilla.getPosicion()==nuevaPosicion){
+    //                 return casilla;
+    //             }
+    //         }
+    //     }
 
-        throw new IllegalArgumentException("La casilla no se encuentra");
+    //     throw new IllegalArgumentException("La casilla no se encuentra");
 
-    }
+    // }
 
 
     /*Método que permite generar un ID para un avatar. Sólo lo usamos en esta clase (por ello es privado).
