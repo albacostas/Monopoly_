a4c2534d18ada8dc362e27109cfc528bda3abf73
Alba Costas <albacostasfernandez@gmail.com>
2024-10-08 18:40:33 +0200

    eliminar comentarios

--- partida/Avatar.java
+++ partida/Avatar.java
@@ -100,29 +100,6 @@ public class Avatar {
         }
     }
 
-
-    // private Casilla ObtenerSiguienteCasilla(ArrayList<ArrayList<Casilla>> casillas, Casilla casillaActual){
-    //     int posActual=casillaActual.getPosicion();
-    //     //Incrementamos la posción una unidad.
-    //     int nuevaPosicion=posActual+1;
-    //     //si la posición excede el número 40 (vuelta completada), regresamos a la casilla 1.
-    //     if(nuevaPosicion>40){
-    //         nuevaPosicion=1;
-    //     } 
-    //     //buscamos la nueva casilla actual basada en la posición
-    //     for(ArrayList<Casilla> lado : casillas){
-    //         for (Casilla casilla: lado){
-    //             if (casilla.getPosicion()==nuevaPosicion){
-    //                 return casilla;
-    //             }
-    //         }
-    //     }
-
-    //     throw new IllegalArgumentException("La casilla no se encuentra");
-
-    // }
-
-
     /*Método que permite generar un ID para un avatar. Sólo lo usamos en esta clase (por ello es privado).
     * El ID generado será una letra mayúscula. Parámetros:
     * - Un arraylist de los avatares ya creados, con el objetivo de evitar que se generen dos ID iguales.
